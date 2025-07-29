f8d6ed65758a242e5c5bf215ee9113f20f055b8b
Joshua Wilfong <joshua14wilfong@gmail.com>
2025-07-29 13:18:35 -0400

    Implements API and database setup
    Sets up the API with Express, including routes for health checks, metrics, user and chirp management, and authentication.
    
    Configures PostgreSQL database connection using `postgres.js` and performs database migrations with Drizzle ORM.
    
    Adds middleware for logging, metrics, and error handling.
    
    Removes static HTML and logo files.
    Updates `.gitignore` to exclude `dist/`, `server.log`, and `.env` files.

---
+++ src/db/schema.ts
@@ -0,0 +1,47 @@
+import { pgTable, timestamp, varchar, uuid } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+  email: varchar("email", { length: 256 }).unique().notNull(),
+  hashedPassword: varchar("hashed_password", { length: 256 })
+    .notNull()
+    .default("unset"),
+});
+
+export type NewUser = typeof users.$inferInsert;
+
+export const chirps = pgTable("chirps", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+  body: varchar("body", { length: 256 }).notNull(),
+  userId: uuid("user_id")
+    .references(() => users.id, { onDelete: "cascade" })
+    .notNull(),
+});
+
+export type NewChirp = typeof chirps.$inferInsert;
+
+export const refreshTokens = pgTable("refresh_tokens", {
+  token: varchar("token", { length: 256 }),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+  userId: uuid("user_id")
+    .references(() => users.id, { onDelete: "cascade" })
+    .notNull(),
+  expiresAt: timestamp("expires_at").notNull(),
+  revokedAt: timestamp("revoked_at"),
+});
+
+export type NewToken = typeof refreshTokens.$inferInsert;
